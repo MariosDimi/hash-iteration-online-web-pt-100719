c3c911a08da150d126a8ab3ca5e90cf549d4da83
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-14 02:22:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,10 +10,4 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_birthday(age)
-  age = age < 12
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
 
